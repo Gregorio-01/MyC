a7b1479b19bb708753b9d9efe47513c5ad06cd14
Demian Ferreyra <demiancra@gmail.com>
2023-04-15 14:46:55 -0300

    [feature] news page, css and functions

--- Client/src/Components/News/components/topNews.jsx
+++ Client/src/Components/News/components/topNews.jsx
@@ -4,8 +4,8 @@ import "./topNews.css"
 
 export const TopNews = () => {
   return (
-  <div className="about-div-top">
-    <h1 className="about-title">Noticias</h1>
+  <div className="news-div-top">
+    <h1 className="about-title">Noticias e información</h1>
       <div className='div-inico-about'>
         <Link to="Home">
             <p className='p-inicio-about'>Inicio</p>
--- Client/src/Components/News/news.jsx
+++ Client/src/Components/News/news.jsx
@@ -1,10 +1,19 @@
+import { ArticleNew } from "./components/articleNew"
 import { TopNews } from "./components/topNews"
+import information from "./data"
 import "./news.css"
 
 const News = () => {
   return (
-    <div className="news-container">
+    <div className="news-component">
       <TopNews />
+      <article className="news-container">
+        {
+          information.map((info) => (
+            <ArticleNew image={info.image} title={info.title} description={info.description} date={info.date} subTitle={info.subTitle} link={info.link}/>
+          ))
+        }
+      </article>
     </div>
   )
 }
