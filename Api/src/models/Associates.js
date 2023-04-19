98a44031d19131b56f9f2f7ed55bb7f6ef042e75
gjuancruz <tatugalaz@gmail.com>
2023-04-19 15:43:32 -0300

    model empresas asociadas

---
+++ Api/src/models/Associates.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const associateSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  banner: {
+    type: String,
+    default: "https://myconstruction.com.ar/wp-content/uploads/2019/02/LOGO-512.png",
+  },
+  photo: {
+    type: String,
+    default: "https://myconstruction.com.ar/wp-content/uploads/2019/02/LOGO-512.png",
+  },
+  product1: {
+    type: String,
+    default: "https://myconstruction.com.ar/wp-content/uploads/2019/02/LOGO-512.png",
+  },
+  product2: {
+    type: String,
+    default: "https://myconstruction.com.ar/wp-content/uploads/2019/02/LOGO-512.png",
+  },
+  product3: {
+    type: String,
+    default: "https://myconstruction.com.ar/wp-content/uploads/2019/02/LOGO-512.png",
+  },
+});
+
+const Associate = mongoose.model('Associate', associateSchema);
+
+export default Associate;
