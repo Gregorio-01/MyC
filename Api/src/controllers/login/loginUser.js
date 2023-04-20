8e107a2efd55799d76e4a02140a320633d5f5fe6
luiseliasdavid <luis.elias.david@gmail.com>
2023-04-20 16:37:12 -0300

    envio de token por headers ok

--- Api/src/controllers/login/loginUser.js
+++ Api/src/controllers/login/loginUser.js
@@ -9,14 +9,13 @@ import { generateJWT } from '../../helpers/generateJWT.js';
 
 
 export const loginUser = async (req, res = response) => {
-  const { mail, password } = req.body
-
-  console.log(mail,password)
+ 
+  const { email , password } = req.body
 
   try {
     //verificar si el modelo existe
-    const usuario = await User.findOne({ email: mail })
-    console.log(usuario)
+    const usuario = await User.findOne({ email: email })
+    
     if (!usuario) {
       return res
         .status(400)
@@ -33,16 +32,18 @@ export const loginUser = async (req, res = response) => {
     //verificar si la contraseña es valida
     
     const validPassword = bcryptjs.compareSync(password, usuario.password)
-    console.log(validPassword)
+    
     if (!validPassword) {
       return res.status(400).json({
         msg: 'Usuario / contraseña invalidos -password',
       })
     }
     const token = await generateJWT(usuario._id)
-    console.log(token)
+    
    /*  const data = await Usuario.findOne({ mail }, '-isActive') */
 /    res.json({
+      name: usuario.name,
+      lastname: usuario.lastname,
       email: usuario.email ,
       token
     })
