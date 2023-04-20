083eeb0baaf49728554a6cf2c276afd8195a49eb
luiseliasdavid <luis.elias.david@gmail.com>
2023-04-19 21:57:27 -0300

    jwt in localstorage when login

--- Client/src/API/usersAPI.js
+++ Client/src/API/usersAPI.js
@@ -1,5 +1,27 @@
 import axios from "axios";
 //aqui se crea un eddpoin base de las rutas de usuario
-export const usersApi = axios.create({
-    baseURL: 'http://localhost:3001/user'
+
+export const loginApi = axios.create({
+    baseURL: 'http://localhost:3001/login'
 })
+
+const userApi = () => {
+    
+    // axios instance
+    let userInstace = axios.create({
+        baseURL: 'http://localhost:3001/user',
+    })
+
+    // Set auth token for any request
+    userInstace.interceptors.request.use(function (config) {
+        
+        const {token} = JSON.parse(localStorage.getItem('userData')) 
+        
+        config.headers.Authorization = token ? `Bearer ${token}` : "";
+        return config;
+    });
+
+    return userInstace;
+};
+
+export default userApi();
