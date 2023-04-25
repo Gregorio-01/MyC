51c3eeb1484941c6892f020beb2ff809c20be251
gjuancruz <tatugalaz@gmail.com>
2023-04-25 18:00:20 -0300

    swiper implementation

--- Client/src/Components/Empresas asociadas/components/main.jsx
+++ Client/src/Components/Empresas asociadas/components/main.jsx
@@ -2,30 +2,53 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAssociates } from "../../../store/slices/associates/thunks";
 import Card from "./card";
+import "./associates.css"
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import 'swiper/css';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar"
 
 const Main = () => {
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getAssociates())
     }, [])
-    const associates = useSelector((state) => state.associates.associates)   
-console.log(associates)
+    const associates = useSelector((state) => state.associates.associates)
+    console.log(associates)
     return (
-        <>
-            {associates?.map(i=>{
-                return(
+        <div className="associates-container">
+            <h1>Empresas Destacadas</h1>
+            <div className="associates-swiper-container">
+                <Swiper
+                    navigation={true} modules={[Navigation]}
+                    spaceBetween={50}
+                    slidesPerView={3}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                </Swiper>
+            </div>
+            {associates?.map(i => {
+                return (
                     <Card
-                    name={i.name}
-                    banner={i.banner}
-                    photo={i.photo}
-                    product1={i.product1}
-                    product2={i.product2}
-                    product3={i.product3}
+                        name={i.name}
+                        banner={i.banner}
+                        photo={i.photo}
+                        product1={i.product1}
+                        product2={i.product2}
+                        product3={i.product3}
                     >
                     </Card>
                 )
             })}
-        </>
+        </div>
     )
 }
 
